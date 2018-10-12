c9dfe18705eb8217293e218bcbf5d52ff7b67b7d
Beling1 <daniel.omola@gmail.com>
2018-10-12 12:55:12 +0200

    Site_V1

---
+++ python.py
@@ -0,0 +1,20 @@
+
+from flask import Flask,render_template
+app=Flask(__name__, template_folder='.')
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+	
+@app.route("/VBA")
+def vba():
+    return render_template("vba.html")
+	
+@app.route("/PYTHON")
+def python():
+    return render_template("python.html")
+	
+@app.route("/CFA")
+def cfa():
+    return render_template("cfa.html")
+	
