9c0ac04264b41e389f97e3ad50313d994848195a
Beling1 <43685205+Beling1@users.noreply.github.com>
2018-10-12 16:33:26 +0200

    Update python.py

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ app=Flask(__name__, template_folder='.')
 
 @app.route("/")
 def index():
-    return render_template("/index.html")
+    return render_template("index.html")
 	
 @app.route("/VBA")
 def vba():
